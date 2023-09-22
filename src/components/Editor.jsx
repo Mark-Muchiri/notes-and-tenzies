97c992b1240ad89e2ca391448d1ed2bbccf3803a
Mark <muchirimark2@gmail.com>
2023-09-22 17:48:42 +0300

    added comments for editor and sidebar files for later reference

--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -4,12 +4,16 @@ import Showdown from "showdown";
 import propTypes from 'prop-types';
 
 export default function Editor({ currentNote, updateNote }) {
+    // Define PropTypes for the component's props
     Editor.propTypes = {
-        currentNote: propTypes,
-        updateNote: propTypes,
+        currentNote: propTypes.object, // Expecting an object for currentNote
+        updateNote: propTypes.func,   // Expecting a function for updateNote
     };
+
+    // Initialize the selectedTab state to "write"
     const [ selectedTab, setSelectedTab ] = useState("write");
 
+    // Create a Showdown converter for rendering Markdown
     const converter = new Showdown.Converter({
         tables: true,
         simplifiedAutoLink: true,
@@ -20,16 +24,19 @@ export default function Editor({ currentNote, updateNote }) {
     return (
         <section className="pane-editor">
             <ReactMde
-                /* Just in-case `curretNote` doesn't exist, 
-                we added `?` before `.` to male it optional
-                */
+                // Set the initial value to the body of the current note (if it exists)
                 value={currentNote?.body}
+                // Callback function to update the note content
                 onChange={updateNote}
+                // Selected tab for write/preview
                 selectedTab={selectedTab}
+                // Callback to change the selected tab
                 onTabChange={setSelectedTab}
+                // Function to generate a Markdown preview
                 generateMarkdownPreview={(markdown) =>
                     Promise.resolve(converter.makeHtml(markdown))
                 }
+                // Set the minimum editor height and height units
                 minEditorHeight={80}
                 heightUnits="vh"
             />
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,27 +1,27 @@
 import propTypes from 'prop-types';
 
 export default function Sidebar(props) {
+    // Define PropTypes for the component's props
     Sidebar.propTypes = {
-        notes: propTypes,
-        currentNote: propTypes,
-        setCurrentNoteId: propTypes,
-        newNote: propTypes,
-        deleteNote: propTypes,
+        notes: propTypes.array,                // Expecting an array for notes
+        currentNote: propTypes.object,        // Expecting an object for currentNote
+        setCurrentNoteId: propTypes.func,      // Expecting a function for setCurrentNoteId
+        newNote: propTypes.func,               // Expecting a function for newNote
+        deleteNote: propTypes.func,            // Expecting a function for deleteNote
     };
-    {/* NOTE => Using split JS func to get the 1st 
-    line of the note and display it on the sidebar */}
 
+    // Create an array of note elements based on the notes data
     const noteElements = props.notes.map((note) => (
         <div key={note.id}>
             <div
-
-                className={`title ${note.id === props.currentNote.id ? "selected-note" : ""
-                    }`}
+                className={`title ${note.id === props.currentNote.id ? "selected-note" : ""}`}
+                // Click event to set the current note ID when clicked
                 onClick={() => props.setCurrentNoteId(note.id)}
             >
-                <h4 className="text-snippet">{note.body.split("\n")[ 0 ]}</h4>
+                <h4 className="text-snippet">{note.body.split("\n")[0]}</h4>
                 <button
                     className="delete-btn"
+                    // Click event to delete the note
                     onClick={() => props.deleteNote(note.id)}
                 >
                     <i className="gg-trash trash-icon"></i>
